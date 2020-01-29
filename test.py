159c7421844e2341c79ffa3ac1763d1ca4067a19
Joseph Hamman <jhamman@ucar.edu>
2020-01-28 16:01:39 -0800

    move to fastapi

---
+++ test.py
@@ -0,0 +1,13 @@
+import uvicorn
+import xarray as xr
+import xpublish
+
+
+if __name__ == "__main__":
+    ds = xr.tutorial.open_dataset('air_temperature',
+                                  chunks=dict(lat=5, lon=5),
+                                  decode_cf=False)
+    print(ds)
+
+    ds.rest.serve()
+    
--- xpublish/rest.py
+++ xpublish/rest.py
@@ -1,12 +1,19 @@
 import dask
 import numpy as np
 import xarray as xr
-from flask import Flask, jsonify
+from fastapi import FastAPI
+from numcodecs import get_codec
 from numcodecs.compat import ensure_ndarray
 from xarray.backends.zarr import _DIMENSION_KEY, _encode_zarr_attr_value
 from xarray.core.pycompat import dask_array_type
 from zarr.storage import array_meta_key, attrs_key, group_meta_key
-from zarr.util import json_dumps, normalize_shape
+from zarr.util import json_dumps, normalize_shape, is_total_slice
+
+from starlette.responses import Response
+import uvicorn
+
+import logging
+
 
 zarr_format = 2
 zarr_consolidated_format = 1
@@ -48,32 +55,42 @@ class RestAccessor:
 
     def make_app(self):
 
-        self._app = Flask(self._name)
+        self._app = FastAPI()
 
-        @self._app.route(f"/{group_meta_key}", methods=["GET"])
+        logger = logging.getLogger("api")
+
+        @self._app.get(f"/{group_meta_key}")
         def get_zgroup():
             return json_dumps(self.get_zgroup())
 
-        @self._app.route(f"/{attrs_key}", methods=["GET"])
+        @self._app.get(f"/{attrs_key}")
         def get_zattrs():
             return json_dumps(self.get_zattrs())
 
-        @self._app.route(f"/{zarr_metadata_key}", methods=["GET"])
+        @self._app.get(f"/{zarr_metadata_key}")
         def get_zmetadata():
-            return json_dumps(self._metadata)
+            return self._metadata
 
-        @self._app.route("/keys", methods=["GET"])
+        @self._app.get("/keys")
         def list_keys():
             return json_dumps(list(self._obj.variables))
 
-        @self._app.route("/<var>/<chunk>", methods=["GET"])
+        @self._app.get("/{var}/{chunk}")
         def get_key(var, chunk):
+            logger.debug('var is %s', var)
+            logger.debug('chunk is %s', chunk)
+
             da = self._obj[var]
             arr_meta = self._metadata["metadata"][f"{var}/{array_meta_key}"]
 
             index = get_indexers(chunk, arr_meta["chunks"])
+            logger.debug(index)
 
             data_chunk = da.data[index]
+            # TODO: need special handling here for edge chunks
+
+            logger.debug('data_chunk.size is %s', data_chunk.size)
+            logger.debug('data_chunk.shape is %s', data_chunk.shape)
 
             if isinstance(data_chunk, dask_array_type):
                 data_chunk = data_chunk.compute()
@@ -88,13 +105,11 @@ class RestAccessor:
                 filters=arr_meta["filters"],
                 compressor=arr_meta["compressor"],
             )
-            return echunk
+            return Response(echunk, media_type='application/octet-stream')
 
-    def serve(self):
-        # app.run(debug=True)
-        from werkzeug.serving import run_simple
-
-        run_simple("localhost", 9000, self._app)
+    def serve(self, host="0.0.0.0", port=9000, log_level='debug', **kwargs):
+        uvicorn.run(self._app, host=host, port=port, log_level=log_level,
+                    **kwargs)
 
 
 def extract_zattrs(da):
