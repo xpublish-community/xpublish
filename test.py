49a69ce0d019999e25e928d1a49d0c44ece03b37
Joseph Hamman <jhamman@ucar.edu>
2020-02-22 15:05:41 -0800

    black

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import xarray as xr
 
 import xpublish  # noqa: F401
 
-if __name__ == '__main__':
-    ds = xr.tutorial.open_dataset('air_temperature', chunks=dict(lat=5, lon=5), decode_cf=False)
+if __name__ == "__main__":
+    ds = xr.tutorial.open_dataset(
+        "air_temperature", chunks=dict(lat=5, lon=5), decode_cf=False
+    )
     print(ds)
 
     ds.rest.serve()
