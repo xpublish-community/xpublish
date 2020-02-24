5ad2969b2427053447e9b2401fed5aa3585aecb8
Joseph Hamman <jhamman@ucar.edu>
2020-02-24 09:39:55 -0800

    start/postBuild

--- .binder/test.py
+++ .binder/test.py
@@ -1,8 +1,15 @@
+from dask.distributed import Client
+
 import xarray as xr
 
 import xpublish  # noqa: F401
 
 if __name__ == "__main__":
+
+    client  = Client()
+    print(client.cluster)
+    print(client.cluster.dashboard_link)
+
     ds = xr.tutorial.open_dataset("air_temperature", chunks=dict(lat=5, lon=5), decode_cf=False)
     print(ds)
 
