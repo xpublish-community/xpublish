b3b09be8f675ca24e07872397ea2010a0c20c4ed
Landung Setiawan <landungs@uw.edu>
2020-02-25 16:25:21 -0800

    Black format code

--- xpublish/rest.py
+++ xpublish/rest.py
@@ -86,7 +86,9 @@ class RestAccessor:
             compressor_config = zjson["metadata"][f"{key}/{array_meta_key}"][
                 "compressor"
             ].get_config()
-            zjson["metadata"][f"{key}/{array_meta_key}"]["compressor"] = compressor_config
+            zjson["metadata"][f"{key}/{array_meta_key}"][
+                "compressor"
+            ] = compressor_config
 
         return zjson
 
@@ -100,14 +102,16 @@ class RestAccessor:
         data_chunk = get_data_chunk(da, chunk, out_shape=arr_meta["chunks"])
 
         echunk = _encode_chunk(
-            data_chunk.tobytes(), filters=arr_meta["filters"], compressor=arr_meta["compressor"],
+            data_chunk.tobytes(),
+            filters=arr_meta["filters"],
+            compressor=arr_meta["compressor"],
         )
         return Response(echunk, media_type="application/octet-stream")
 
     def init_app(self, **kwargs):
-        
+
         self._app = FastAPI(**kwargs)
-        
+
         @self._app.get(f"/{zarr_metadata_key}")
         def get_zmetadata():
             return self.zmetadata_json()
@@ -147,6 +151,7 @@ class RestAccessor:
                 xr.show_versions(f)
                 versions = f.getvalue()
             return versions
+
         return self._app
 
     @property
@@ -203,7 +208,9 @@ def _extract_fill_value(da, dtype):
 def extract_zarray(da, encoding, dtype):
     """ helper function to extract zarr array metadata. """
     meta = {
-        "compressor": encoding.get("compressor", da.encoding.get("compressor", default_compressor)),
+        "compressor": encoding.get(
+            "compressor", da.encoding.get("compressor", default_compressor)
+        ),
         "filters": encoding.get("filters", da.encoding.get("filters", None)),
         "chunks": encoding.get("chunks", None),
         "dtype": dtype.str,
