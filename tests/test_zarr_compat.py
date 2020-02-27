6dffc270369d4c5051d16fd429ee78b84d1f991c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-02-27 08:34:14 -0700

    Add more tests

--- tests/test_zarr_compat.py
+++ tests/test_zarr_compat.py
@@ -5,12 +5,12 @@ import xarray as xr
 
 import xpublish  # noqa: F401
 
-from .utils import TestMapper
+from .utils import TestMapper, create_dataset
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def airtemp_ds():
-    ds = xr.tutorial.open_dataset("air_temperature")
+    ds = xr.tutorial.open_dataset('air_temperature')
     return ds.chunk(dict(ds.dims))
 
 
@@ -18,13 +18,91 @@ def test_zmetadata_identical(airtemp_ds):
     zarr_dict = {}
     airtemp_ds.to_zarr(zarr_dict, consolidated=True)
     mapper = TestMapper(airtemp_ds.rest.app)
-    actual = json.loads(mapper[".zmetadata"].decode())
-    expected = json.loads(zarr_dict[".zmetadata"].decode())
+    actual = json.loads(mapper['.zmetadata'].decode())
+    expected = json.loads(zarr_dict['.zmetadata'].decode())
     assert actual == expected
 
 
-def test_roundtrip(airtemp_ds):
-    mapper = TestMapper(airtemp_ds.rest.app)
+@pytest.mark.parametrize(
+    'start, end, freq, nlats, nlons, var_const, calendar, use_cftime',
+    [
+        ('2018-01-01', '2021-01-01', 'MS', 180, 360, True, 'standard', False),
+        ('2018-01-01', '2021-01-01', 'D', 180, 360, False, 'noleap', True),
+        ('2018-01-01', '2021-01-01', '6H', 180, 360, True, 'gregorian', False),
+        ('2018-01-01', '2050-01-01', 'A', 180, 360, None, '360_day', True),
+    ],
+)
+def test_roundtrip(start, end, freq, nlats, nlons, var_const, calendar, use_cftime):
+    ds = create_dataset(
+        start=start,
+        end=end,
+        nlats=nlats,
+        nlons=nlons,
+        var_const=var_const,
+        use_cftime=use_cftime,
+        calendar=calendar,
+    )
+    ds = ds.chunk(ds.dims)
+    mapper = TestMapper(ds.rest.app)
     actual = xr.open_zarr(mapper, consolidated=True)
 
-    xr.testing.assert_identical(actual, airtemp_ds)
+    xr.testing.assert_identical(actual, ds)
+
+
+@pytest.mark.xfail(reason='Custom chunking not working properly')
+@pytest.mark.parametrize(
+    'start, end, freq, nlats, nlons, var_const, calendar, use_cftime, chunks',
+    [
+        ('2018-01-01', '2021-01-01', 'MS', 180, 360, True, 'standard', False, {'time': 10}),
+        (
+            '2018-01-01',
+            '2021-01-01',
+            'D',
+            300,
+            600,
+            False,
+            'noleap',
+            True,
+            {'time': 10, 'lat': 300, 'lon': 300},
+        ),
+        (
+            '2018-01-01',
+            '2021-01-01',
+            '12H',
+            180,
+            360,
+            True,
+            'gregorian',
+            False,
+            {'time': 36, 'lat': 10},
+        ),
+        (
+            '2018-01-01',
+            '2050-01-01',
+            'A',
+            300,
+            600,
+            None,
+            '360_day',
+            True,
+            {'time': 10, 'lat': 30, 'lon': 30},
+        ),
+    ],
+)
+def test_roundtrip_custom_chunks(
+    start, end, freq, nlats, nlons, var_const, calendar, use_cftime, chunks
+):
+    ds = create_dataset(
+        start=start,
+        end=end,
+        nlats=nlats,
+        nlons=nlons,
+        var_const=var_const,
+        use_cftime=use_cftime,
+        calendar=calendar,
+    )
+    ds = ds.chunk(chunks)
+    mapper = TestMapper(ds.rest.app)
+    actual = xr.open_zarr(mapper, consolidated=True)
+
+    xr.testing.assert_identical(actual, ds)
--- xpublish/rest.py
+++ xpublish/rest.py
@@ -20,12 +20,12 @@ from zarr.util import normalize_shape
 
 zarr_format = 2
 zarr_consolidated_format = 1
-zarr_metadata_key = ".zmetadata"
+zarr_metadata_key = '.zmetadata'
 
-logger = logging.getLogger("api")
+logger = logging.getLogger('api')
 
 
-@xr.register_dataset_accessor("rest")
+@xr.register_dataset_accessor('rest')
 class RestAccessor:
     """ REST API Accessor
 
@@ -48,18 +48,18 @@ class RestAccessor:
 
     def _get_zmetadata(self):
         """ helper method to create consolidated zmetadata dictionary """
-        zmeta = {"zarr_consolidated_format": zarr_consolidated_format, "metadata": {}}
-        zmeta["metadata"][group_meta_key] = {"zarr_format": zarr_format}
-        zmeta["metadata"][attrs_key] = self._get_zattrs()
+        zmeta = {'zarr_consolidated_format': zarr_consolidated_format, 'metadata': {}}
+        zmeta['metadata'][group_meta_key] = {'zarr_format': zarr_format}
+        zmeta['metadata'][attrs_key] = self._get_zattrs()
 
         for key, da in self._obj.variables.items():
             # encode variable
             encoded_da = encode_zarr_variable(da)
             self._variables[key] = encoded_da
             self._encoding[key] = _extract_zarr_variable_encoding(da)
-            zmeta["metadata"][f"{key}/{attrs_key}"] = extract_zattrs(encoded_da)
-            zmeta["metadata"][f"{key}/{array_meta_key}"] = extract_zarray(
-                encoded_da, self._encoding.get(key, {}), da.encoding["dtype"]
+            zmeta['metadata'][f'{key}/{attrs_key}'] = extract_zattrs(encoded_da)
+            zmeta['metadata'][f'{key}/{array_meta_key}'] = extract_zarray(
+                encoded_da, self._encoding.get(key, {}), da.encoding.get('dtype', encoded_da.dtype)
             )
 
         return zmeta
@@ -83,26 +83,26 @@ class RestAccessor:
         zjson = copy.deepcopy(self.zmetadata)
         for key in list(self._obj.variables):
             # convert compressor to dict
-            compressor_config = zjson["metadata"][f"{key}/{array_meta_key}"][
-                "compressor"
+            compressor_config = zjson['metadata'][f'{key}/{array_meta_key}'][
+                'compressor'
             ].get_config()
-            zjson["metadata"][f"{key}/{array_meta_key}"]["compressor"] = compressor_config
+            zjson['metadata'][f'{key}/{array_meta_key}']['compressor'] = compressor_config
 
         return zjson
 
     async def get_key(self, var, chunk):
-        logger.debug("var is %s", var)
-        logger.debug("chunk is %s", chunk)
+        logger.debug('var is %s', var)
+        logger.debug('chunk is %s', chunk)
 
         da = self._variables[var].data
-        arr_meta = self.zmetadata["metadata"][f"{var}/{array_meta_key}"]
+        arr_meta = self.zmetadata['metadata'][f'{var}/{array_meta_key}']
 
-        data_chunk = get_data_chunk(da, chunk, out_shape=arr_meta["chunks"])
+        data_chunk = get_data_chunk(da, chunk, out_shape=arr_meta['chunks'])
 
         echunk = _encode_chunk(
-            data_chunk.tobytes(), filters=arr_meta["filters"], compressor=arr_meta["compressor"],
+            data_chunk.tobytes(), filters=arr_meta['filters'], compressor=arr_meta['compressor'],
         )
-        return Response(echunk, media_type="application/octet-stream")
+        return Response(echunk, media_type='application/octet-stream')
 
     def init_app(
         self,
@@ -150,20 +150,20 @@ class RestAccessor:
             **kwargs,
         )
 
-        @self._app.get(f"/{zarr_metadata_key}")
+        @self._app.get(f'/{zarr_metadata_key}')
         def get_zmetadata():
             return self.zmetadata_json()
 
-        @self._app.get("/keys")
+        @self._app.get('/keys')
         def list_keys():
             return list(self._obj.variables)
 
-        @self._app.get("/")
+        @self._app.get('/')
         def repr():
-            with xr.set_options(display_style="html"):
+            with xr.set_options(display_style='html'):
                 return HTMLResponse(self._obj._repr_html_())
 
-        @self._app.get("/info")
+        @self._app.get('/info')
         def info():
             import io
 
@@ -172,16 +172,16 @@ class RestAccessor:
                 info = buffer.getvalue()
             return info
 
-        @self._app.get("/dict")
+        @self._app.get('/dict')
         def to_dict(data: bool = False):
             return self._obj.to_dict(data=data)
 
-        @self._app.get("/{var}/{chunk}")
+        @self._app.get('/{var}/{chunk}')
         async def get_key(var, chunk):
             result = await self.get_key(var, chunk)
             return result
 
-        @self._app.get("/versions")
+        @self._app.get('/versions')
         def versions():
             import io
 
@@ -199,7 +199,7 @@ class RestAccessor:
             self.init_app()
         return self._app
 
-    def serve(self, host="0.0.0.0", port=9000, log_level="debug", **kwargs):
+    def serve(self, host='0.0.0.0', port=9000, log_level='debug', **kwargs):
         """ Serve this app via ``uvicorn.run``.
 
         Parameters
@@ -230,34 +230,34 @@ def extract_zattrs(da):
 
     # We don't want `_FillValue` in `.zattrs`
     # It should go in `fill_value` section of `.zarray`
-    _ = zattrs.pop("_FillValue", None)
+    _ = zattrs.pop('_FillValue', None)
 
     return zattrs
 
 
 def _extract_fill_value(da, dtype):
     """ helper function to extract fill value from DataArray. """
-    fill_value = da.attrs.pop("_FillValue", None)
+    fill_value = da.attrs.pop('_FillValue', None)
     return encode_fill_value(fill_value, dtype)
 
 
 def extract_zarray(da, encoding, dtype):
     """ helper function to extract zarr array metadata. """
     meta = {
-        "compressor": encoding.get("compressor", da.encoding.get("compressor", default_compressor)),
-        "filters": encoding.get("filters", da.encoding.get("filters", None)),
-        "chunks": encoding.get("chunks", None),
-        "dtype": dtype.str,
-        "fill_value": _extract_fill_value(da, dtype),
-        "order": "C",
-        "shape": list(normalize_shape(da.shape)),
-        "zarr_format": zarr_format,
+        'compressor': encoding.get('compressor', da.encoding.get('compressor', default_compressor)),
+        'filters': encoding.get('filters', da.encoding.get('filters', None)),
+        'chunks': encoding.get('chunks', None),
+        'dtype': dtype.str,
+        'fill_value': _extract_fill_value(da, dtype),
+        'order': 'C',
+        'shape': list(normalize_shape(da.shape)),
+        'zarr_format': zarr_format,
     }
-    if meta["chunks"] is None:
+    if meta['chunks'] is None:
         if da.chunks is not None:
-            meta["chunks"] = list([c[0] for c in da.chunks])
+            meta['chunks'] = list([c[0] for c in da.chunks])
         else:
-            meta["chunks"] = list(da.shape)
+            meta['chunks'] = list(da.shape)
 
     return meta
 
@@ -271,7 +271,7 @@ def _encode_chunk(chunk, filters=None, compressor=None):
 
     # check object encoding
     if ensure_ndarray(chunk).dtype == object:
-        raise RuntimeError("cannot write object array without object codec")
+        raise RuntimeError('cannot write object array without object codec')
 
     # compress
     if compressor:
@@ -288,13 +288,13 @@ def get_data_chunk(da, chunk_id, out_shape):
 
     If this is an incomplete edge chunk, pad the returned array to match out_shape.
     """
-    ikeys = tuple(map(int, chunk_id.split(".")))
+    ikeys = tuple(map(int, chunk_id.split('.')))
     try:
         chunk_data = da.blocks[ikeys]
     except:
         chunk_data = np.asarray(da)
 
-    logger.debug("checking chunk output size, %s == %s" % (chunk_data.shape, out_shape))
+    logger.debug('checking chunk output size, %s == %s' % (chunk_data.shape, out_shape))
 
     if isinstance(chunk_data, dask_array_type):
         chunk_data = chunk_data.compute()
