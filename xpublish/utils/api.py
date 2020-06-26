fef589ca9787f55a9805288d0bb1e06269eeba06
Benoit Bovy <benbovy@gmail.com>
2020-06-26 14:50:27 +0200

    add a new "routers" parameter to Dataset.rest()

---
+++ xpublish/utils/api.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+
+def normalize_app_routers(routers):
+
+    new_routers = []
+
+    for rt in routers:
+        if isinstance(rt, APIRouter):
+            new_routers.append(rt, {})
+        elif isinstance(rt, tuple) and isinstance(rt[0], APIRouter) and len(rt) == 2:
+            new_routers.append(rt)
+        else:
+            raise ValueError(
+                "Invalid format for routers item, please provide either an APIRouter "
+                "instance or a (APIRouter, {...}) tuple."
+            )
+
+    return new_routers
