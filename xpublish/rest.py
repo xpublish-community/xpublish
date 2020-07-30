130a1fac02e9dc29a72d6c56e4583326a3b6b6c7
Benoit Bovy <benbovy@gmail.com>
2020-07-30 11:35:28 +0200

    docstrings tweaks

--- xpublish/rest.py
+++ xpublish/rest.py
@@ -10,7 +10,7 @@ from .utils.api import check_route_conflicts, normalize_app_routers
 
 @xr.register_dataset_accessor('rest')
 class RestAccessor:
-    """ REST API Accessor
+    """REST API Accessor
 
     Parameters
     ----------
@@ -21,6 +21,7 @@ class RestAccessor:
     -----
     When using this as an accessor on an Xarray.Dataset, options are set via
     the ``RestAccessor.__call__()`` method.
+
     """
 
     def __init__(self, xarray_obj):
@@ -41,8 +42,7 @@ class RestAccessor:
         self._initialized = False
 
     def __call__(self, routers=None, cache_kws=None, app_kws=None):
-        """
-        Initialize this RestAccessor by setting optional configuration values
+        """Initialize this accessor by setting optional configuration values.
 
         Parameters
         ----------
@@ -51,18 +51,20 @@ class RestAccessor:
             fastAPI application. If None, the default routers will be included.
             The items of the list may also be tuples with the following format:
             ``[(router1, {'prefix': '/foo', 'tags': ['foo', 'bar']})]``.
-            The 1st tuple element is a ``APIRouter`` instance and the 2nd element
-            is a dictionary that is used to pass keyword arguments to
+            The 1st tuple element is a :class:`fastapi.APIRouter` instance and the
+            2nd element is a dictionary that is used to pass keyword arguments to
             :meth:`fastapi.FastAPI.include_router`.
         cache_kws : dict, optional
-            Dictionary of keyword arguments to be passed to ``cachey.Cache()``
+            Dictionary of keyword arguments to be passed to
+            :meth:`cachey.Cache.__init__()`.
         app_kws : dict, optional
             Dictionary of keyword arguments to be passed to
-            ``fastapi.FastAPI()``
+            :meth:`fastapi.FastAPI.__init__()`.
 
         Notes
         -----
         This method can only be invoked once.
+
         """
         if self._initialized:
             raise RuntimeError('This accessor has already been initialized')
@@ -79,15 +81,15 @@ class RestAccessor:
         return self
 
     @property
-    def cache(self):
-        """ Cache Property """
+    def cache(self) -> cachey.Cache:
+        """Returns the :class:`cachey.Cache` instance used by the FastAPI application."""
 
         if self._cache is None:
             self._cache = cachey.Cache(**self._cache_kws)
         return self._cache
 
     def _init_app(self):
-        """ Initiate FastAPI Application. """
+        """Initiate the FastAPI application."""
 
         self._app = FastAPI(**self._app_kws)
 
@@ -100,14 +102,14 @@ class RestAccessor:
         return self._app
 
     @property
-    def app(self):
-        """ FastAPI app """
+    def app(self) -> FastAPI:
+        """Returns the :class:`fastapi.FastAPI` application instance."""
         if self._app is None:
             self._app = self._init_app()
         return self._app
 
     def serve(self, host='0.0.0.0', port=9000, log_level='debug', **kwargs):
-        """ Serve this app via ``uvicorn.run``.
+        """Serve this FastAPI application via :func:`uvicorn.run`.
 
         Parameters
         ----------
@@ -119,10 +121,11 @@ class RestAccessor:
             App logging level, valid options are
             {'critical', 'error', 'warning', 'info', 'debug', 'trace'}.
         kwargs :
-            Additional arguments to be passed to ``uvicorn.run``.
+            Additional arguments to be passed to :func:`uvicorn.run`.
 
         Notes
         -----
         This method is blocking and does not return.
+
         """
         uvicorn.run(self.app, host=host, port=port, log_level=log_level, **kwargs)
