b0dc9d01d55518dee0f74bc59e23a84e9960856e
maif <Iacopo.Ferrario@ecmwf.int>
2021-08-10 12:45:08 +0100

    fix import error

--- xpublish/routers/__init__.py
+++ xpublish/routers/__init__.py
@@ -1,4 +1,8 @@
 from .base import base_router
 from .common import common_router, dataset_collection_router
 from .zarr import zarr_router
-from .xyz import xyz_router
+
+try:
+    from .xyz import xyz_router
+except ImportError:
+    pass
