1c8cecfb534f7539bb505562d4c2d1fd2f12c44c
Kyle Wilcox <kyle@axiomdatascience.com>
2023-03-08 11:36:46 -0800

    Ability to overwrite existing plugins
    This allows someone to load the default plugins and overwrite those that
    they wish to by specifying an `overwrite=True` parameter to
    `reguster_plugin`.

--- tests/test_plugin_management.py
+++ tests/test_plugin_management.py
@@ -1,3 +1,4 @@
+import pytest
 from starlette.testclient import TestClient
 
 from xpublish import Rest
@@ -30,6 +31,46 @@ def test_configure_plugins(airtemp_ds):
     client = TestClient(app)
 
     info_response = client.get('/datasets/airtemp/meta/info')
+    json_response = info_response.json()
+    assert json_response['dimensions'] == airtemp_ds.dims
+    assert list(json_response['variables'].keys()) == list(airtemp_ds.variables.keys())
+
+
+def test_overwrite_plugins(airtemp_ds):
+    from xpublish.plugins.included.dataset_info import DatasetInfoPlugin
+
+    # Test discoverable plugins
+    info = DatasetInfoPlugin()
+    rest = Rest({'airtemp': airtemp_ds}, plugins={'info': info})
+
+    # Registering a duplicate plugin name will fail
+    same_name = DatasetInfoPlugin(name='info', dataset_router_prefix='/newinfo')
+    with pytest.raises(ValueError):
+        rest.register_plugin(same_name)
+
+    # Register with overwrite
+    rest.register_plugin(same_name, overwrite=True)
+
+    # Change the name of the plugin
+    new_name = DatasetInfoPlugin(name='meta', dataset_router_prefix='/newmeta')
+    rest.register_plugin(new_name)
+
+    # Registering a duplicate plugin name will fail
+    new_prefix = DatasetInfoPlugin(name='meta', dataset_router_prefix='/meta')
+    with pytest.raises(ValueError):
+        rest.register_plugin(new_prefix)
+
+    app = rest.app
+    client = TestClient(app)
+
+    # Original plugin shouldn't respond... it's gone
+    assert client.get('/datasets/airtemp/info').status_code == 404
+
+    # # Original plugin shouldn't respond... it's gone
+    # assert client.get('/datasets/airtemp/meta').status_code == 200
+
+    # New plugin should respond correctly
+    info_response = client.get('/datasets/airtemp/newmeta/info')
     json_response = info_response.json()
     assert json_response['dimensions'] == airtemp_ds.dims
     assert list(json_response['variables'].keys()) == list(airtemp_ds.variables.keys())
