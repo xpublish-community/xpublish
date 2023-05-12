fc366a0218f35c26c5f3b3e5ad200620bce794e1
Alex Kerney <abk@mac.com>
2023-05-12 08:46:38 -0400

    Shorten the docs title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -167,10 +167,10 @@ html_context = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = f'{project} {version}'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = f'{project} {version}'
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
