4ad6e5ac81478f89910eb368a643dbf72fbb1fa3
Alex Kerney <abk@mac.com>
2023-05-12 16:13:53 -0400

    Test on 3.11 and add to classifiers

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'Topic :: Scientific/Engineering',
 ]
 
