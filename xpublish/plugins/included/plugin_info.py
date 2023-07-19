84df4861c9234300c9eb50864346547eb92f1162
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-07-19 12:04:26 -0400

    Add annotation for plugin_info, one typehint, and small readability changes

--- xpublish/plugins/included/plugin_info.py
+++ xpublish/plugins/included/plugin_info.py
@@ -18,14 +18,17 @@ class PluginInfo(BaseModel):
 class PluginInfoPlugin(Plugin):
     """Expose plugin source and version"""
 
-    name = 'plugin_info'
+    name: str = 'plugin_info'
 
     app_router_prefix: str = ''
     app_router_tags: Sequence[str] = ['plugin_info']
 
     @hookimpl
-    def app_router(self, deps: Dependencies):
-        router = APIRouter(prefix=self.app_router_prefix, tags=list(self.app_router_tags))
+    def app_router(self, deps: Dependencies) -> APIRouter:
+        router = APIRouter(
+            prefix=self.app_router_prefix,
+            tags=list(self.app_router_tags),
+        )
 
         @router.get('/plugins')
         def get_plugins(
@@ -44,7 +47,8 @@ class PluginInfoPlugin(Plugin):
                     version = None  # pragma: no cover
 
                 plugin_info[name] = PluginInfo(
-                    path=f'{plugin_type.__module__}.{plugin.__repr_name__()}', version=version
+                    path=f'{plugin_type.__module__}.{plugin.__repr_name__()}',
+                    version=version,
                 )
 
             return plugin_info
